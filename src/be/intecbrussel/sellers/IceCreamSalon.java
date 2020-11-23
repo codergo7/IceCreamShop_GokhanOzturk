b2a7b30cc3076b5ab710805fbecce50d380d3d0f
codergo7 <codergo7@gmail.com>
2020-11-23 13:45:35 +0100

    implemented order methods

--- src/be/intecbrussel/sellers/IceCreamSalon.java
+++ src/be/intecbrussel/sellers/IceCreamSalon.java
@@ -9,26 +9,28 @@ public class IceCreamSalon implements IceCreamSeller {
     private PriceList priceList;
     private double totalProfit;
 
-    public IceCreamSalon() {
-    }
-
     public IceCreamSalon(PriceList priceList) {
         this.priceList = priceList;
     }
 
     @Override
-    public Cone orderCone(Cone.Flavor[] balls) {
-        return null;
+    public Cone orderCone(Cone.Flavor... balls) {
+        if(balls!=null && priceList!=null){
+            totalProfit += balls.length * priceList.getBallPrice();
+        }
+        return new Cone(balls);
     }
 
     @Override
     public IceRocket orderIceRocket() {
-        return null;
+        totalProfit += priceList.getRocketPrice();
+        return new IceRocket();
     }
 
     @Override
     public Magnum orderMagnum(Magnum.MagnumType magnumType) {
-        return null;
+        totalProfit += priceList.getMagnumPrice(magnumType);
+        return new Magnum(magnumType);
     }
 
     @Override
